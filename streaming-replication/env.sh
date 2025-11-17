c9824217b000ecce025ec5d2f9d2ce4434f740c1
rigelia1911 <zephyrian736@gmail.com>
2025-11-17 12:09:44 +0700

    Add files via upload

---
+++ streaming-replication/env.sh
@@ -0,0 +1,7 @@
+alias dcu="sudo docker-compose up -d"
+alias dcd="sudo docker-compose down"
+alias dps="sudo docker ps"
+alias der="sudo docker exec -it streaming-replication-postgres_replica-1 bash"
+alias dep="sudo docker exec -it streaming-replication-postgres_primary-1 bash"
+alias dlr="sudo docker logs streaming-replication-postgres_replica-1"
+alias dlp="sudo docker logs streaming-replication-postgres_primary-1"
